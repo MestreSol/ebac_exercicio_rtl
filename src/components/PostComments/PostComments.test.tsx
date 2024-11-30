ebc14f717cdc51b20c1adc2b63e333cd5f0f1fe8
MestreSol <mb.muitobosta@gmail.com>
2024-11-30 12:51:59 -0300

    Add Test

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,38 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComments from "."; // Importo o componente
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+//mock 3 comments   
+const comments = ["Primeiro comentário", "Segundo comentário", "Terceiro comentário"];
+
+describe("Comments", () => {
+    test("Comment form exists", () => {
+        render(<PostComments />);
+        const commentArea = screen.getByTestId("post-comments");
+        expect(commentArea).toBeInTheDocument();
+    });
+
+    test("Should add a comment", () => {
+        render(<PostComments />);
+        const commentTextarea = screen.getByRole("textbox");
+        fireEvent.change(commentTextarea, {
+            target: { value: "First comment" },
+        });
+        expect(commentTextarea).toHaveValue("First comment");
+        const submitButton = screen.getByRole("button", { name: /comentar/i });
+        fireEvent.click(submitButton);
+        expect(screen.getByText("First comment")).toBeInTheDocument();
+    });
+
+    test("Should add three comments", () => {
+        render(<PostComments />);
+        const commentTextarea = screen.getByRole("textbox");
+        comments.forEach((comment) => {
+            fireEvent.change(commentTextarea, {
+                target: { value: comment },
+            });
+            fireEvent.click(screen.getByRole("button", { name: /comentar/i }));
+            expect(screen.getByText(comment)).toBeInTheDocument();
+        });
     });
 });
